ed8ef7eb9ac1d24a2ad165cb51ec4d6447e8b41b
Rafael Aybulatov <rafaelajbulatov@gmail.com>
2023-12-29 03:26:48 +0300

    updated an error message

--- hello_triangle_app/HelloTriangleApplication.cpp
+++ hello_triangle_app/HelloTriangleApplication.cpp
@@ -1242,7 +1242,7 @@ VKAPI_ATTR VkBool32 VKAPI_CALL HelloTriangleApplication::debugCallback(
         const VkDebugUtilsMessengerCallbackDataEXT* pCallbackData,
         void* pUserData)
 {
-    std::cerr << "validation layer: " << pCallbackData->pMessage << std::endl;
+    std::cerr << pCallbackData->pMessage << std::endl;
 
     return VK_FALSE;
 }
